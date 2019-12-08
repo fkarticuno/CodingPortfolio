026c226ab8ef99dc789cae77e706e3d45ba71cdb
Frederick Articuno <mckiyoshi@gmail.com>
2019-12-08 17:06:13 -0500

    added selfie with resume clickable

--- Assets/indexscript.js
+++ Assets/indexscript.js
@@ -1,6 +1,6 @@
 console.log("linked")
 var title = [
-    "skipped",
+    "00-RESUME",
     "01-HTML CSS GIT",
     "02-CSS BOOTSTRAP",
     "03-JAVASCRIPT",
@@ -21,7 +21,7 @@ var title = [
     "18-MONGO & MONGOOSE"
 ]
 var desc = [
-    "skipped",
+    "This document outlines my work history and is used in tandem with my coding portfolio to cover more ground.",
     "In this assignment, I built a professional portfolio site using HTML/CSS. These instructions include a short style guide; follow it to design an aesthetically pleasing layout while creating different kinds of complex HTML elements.",
     "In this assignment, I created two different portfolios. The first will be building my portfolio layout using the Bootstrap CSS Framework. The second will be enhancing the portfolio I made last unit with a mobile-responsive layout.",
     "In this assignment, I created one of two possible computer games: Word Guess or Psychic. These apps will run in the browser, and feature dynamically updated HTML and CSS powered by my JavaScript code.",
